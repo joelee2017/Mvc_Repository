7201f746c878378f4e900a90ef654b8388d15a26
joe lee <as955133@gmail.com>
2018-05-01 18:39:42 +0800

    ICategoryRepository 實作 CategoryRepository  實作

---
+++ Mvc_Repository/Models/Repositiory/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_Repository.Models.Repositiory
+{
+    public class ProductRepository
+    {
+    }
+}
